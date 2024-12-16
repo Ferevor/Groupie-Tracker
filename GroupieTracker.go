30ffcc9b27b7ae06a13a4a79a4ce8574e7e26dd9
Smith06S <sarahelenasmith@.com>
2024-12-16 19:36:30 +0100

    create all the functions to get all the info in the api but their all the same so maybe create just one function that does it all in one go

--- GroupieTracker.go
+++ GroupieTracker.go
@@ -8,70 +8,104 @@ import (
 	"net/http"
 )
 
-type artists struct {
-	id            int
-	image         string
-	name          string
-	members       []string
-	creationDates int
-	firstAlbum    string
-	location      locations `json: "location"`
-	concertDates  dates     `json: "concertDates"`
-	relations     relation  `json: "relations"`
+type Artist struct {
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Location     string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
 }
 
-type locations struct {
-	id        int
-	locations []string
-	dates     dates `json: "dates"`
+type Locations struct {
+	Id        int      `json:"id"`
+	Locations []string `json:"locations"`
+	Dates     string   `json:"dates"`
 }
 
-type dates struct {
-	id    int
-	dates []string
+type Dates struct {
+	Id    int      `json:"id"`
+	Dates []string `json:"dates"`
 }
 
-type relation struct {
-	id             int
-	datesLocations []string `json: "datesLocations"`
+type Relation struct {
+	Id             int      `json:"id"`
+	DatesLocations []string `json:"datesLocations"`
 }
 
-func GetLocations() {
-	res := GetAPI("https://groupietrackers.herokuapp.com/api/locations")
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var location locations
-	json.Unmarshal(body, &location)
-
-}
-
-func GetDates() {
-	res := GetAPI("https://groupietrackers.herokuapp.com/api/dates")
-	fmt.Println(res)
-}
-
-func GetArtist() {
-	res := GetAPI("https://groupietrackers.herokuapp.com/api/artists")
-	fmt.Println(res)
-}
-
-func GetRelation() {
-	res := GetAPI("https://groupietrackers.herokuapp.com/api/dates")
-	fmt.Println(res)
-}
-
-func GetAPI(url string) *http.Response {
-	res, err := http.Get(url)
+func GetInfo(url string) []byte {
+	res, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer res.Body.Close()
-	return res
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return body
+}
+
+func GetArtist() {
+	body := GetInfo("https://groupietrackers.herokuapp.com/api/artists")
+
+	var artists []Artist
+	err := json.Unmarshal(body, &artists)
+	if err != nil {
+		log.Fatalf("Error unmarshaling data: %v", err)
+	}
+
+	for _, artist := range artists {
+		fmt.Println(artist.Name)
+	}
+}
+
+func GetLocations() {
+	body := GetInfo("https://groupietrackers.herokuapp.com/api/locations")
+
+	var locations []Locations
+	err := json.Unmarshal(body, &locations)
+	if err != nil {
+		log.Fatalf("Error unmarshaling data: %v", err)
+	}
+
+	for _, location := range locations {
+		fmt.Println(location.Dates)
+	}
+}
+
+func GetDates() {
+	body := GetInfo("https://groupietrackers.herokuapp.com/api/dates")
+
+	var dates []Dates
+	err := json.Unmarshal(body, &dates)
+	if err != nil {
+		log.Fatalf("Error unmarshaling data: %v", err)
+	}
+
+	for _, date := range dates {
+		fmt.Println(date.Dates)
+	}
+}
+
+func GetRelation() {
+	body := GetInfo("https://groupietrackers.herokuapp.com/api/dates")
+
+	var relation []Relation
+	err := json.Unmarshal(body, &relation)
+	if err != nil {
+		log.Fatalf("Error unmarshaling data: %v", err)
+	}
+
+	for _, relate := range relation {
+		fmt.Println(relate.DatesLocations)
+	}
 }
 
 func main() {
-
+	GetArtist()
 }
