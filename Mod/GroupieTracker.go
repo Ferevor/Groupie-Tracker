8b3eaf5a5c403db34232a896c1631d35972905ee
ER1707 <ewan.17.remo@gmail.com>
2025-01-23 12:28:25 +0100

    changement html/css

---
+++ Mod/GroupieTracker.go
@@ -0,0 +1,140 @@
+package Mod
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Artist struct {
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Location     string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
+type LocationData struct {
+	Id        int      `json:"id"`
+	Locations []string `json:"locations"`
+	Dates     Dates    `json:"dates"`
+}
+
+type Dates struct {
+	Id    int      `json:"id"`
+	Dates []string `json:"dates"`
+}
+
+type Relation struct {
+	Id             int                 `json:"id"`
+	DatesLocations map[string][]string `json:"datesLocations"`
+}
+
+func GetInfo(url string) []byte {
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return body
+}
+
+func GetArtist() ([]Artist, error) {
+	url := "https://groupietrackers.herokuapp.com/api/artists"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Erreur lors de la requête GET: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Erreur lors de la lecture du corps de la réponse: %v", err)
+	}
+
+	var artists []Artist
+	err = json.Unmarshal(body, &artists)
+	if err != nil {
+		return nil, fmt.Errorf("Erreur lors du déchiffrement du JSON: %v", err)
+	}
+
+	return artists, nil
+}
+
+func GetLocations() {
+	body := GetInfo("https://groupietrackers.herokuapp.com/api/locations")
+
+	// Unmarshal into a single Locations struct to inspect the structure
+	var locations LocationData
+	err := json.Unmarshal(body, &locations)
+	if err != nil {
+		log.Fatalf("Error unmarshaling data: %v", err)
+	}
+
+	locationJSON, err := json.MarshalIndent(locations, "", "  ")
+	if err != nil {
+		log.Fatalf("Error marshaling data: %v", err)
+	}
+	fmt.Println(string(locationJSON))
+	fmt.Println("------------------------------")
+}
+
+func GetDates() {
+	body := GetInfo("https://groupietrackers.herokuapp.com/api/dates")
+
+	fmt.Println("Raw JSON Response:")
+	fmt.Println(string(body))
+
+	var dates Dates
+	err := json.Unmarshal(body, &dates)
+	if err != nil {
+		log.Fatalf("Error unmarshaling data: %v", err)
+	}
+
+	dateJSON, err := json.MarshalIndent(dates, "", "  ")
+	if err != nil {
+		log.Fatalf("Error marshaling data: %v", err)
+	}
+	fmt.Println(string(dateJSON))
+	fmt.Println("------------------------------")
+}
+
+func GetRelation() {
+	body := GetInfo("https://groupietrackers.herokuapp.com/api/relation")
+
+	fmt.Println("Raw JSON Response:")
+	fmt.Println(string(body))
+
+	var relation Relation
+	err := json.Unmarshal(body, &relation)
+	if err != nil {
+		log.Fatalf("Error unmarshaling data: %v", err)
+	}
+
+	// Convert the relation struct to a JSON string with indentation
+	relationJSON, err := json.MarshalIndent(relation, "", "  ")
+	if err != nil {
+		log.Fatalf("Error marshaling data: %v", err)
+	}
+	fmt.Println(string(relationJSON))
+	fmt.Println("------------------------------")
+}
+
+//	func main() {
+//		GetArtist()
+//		//GetLocations()
+//		//GetDates()
+//		//GetRelation()
+//	}
--- main.go
+++ main.go
@@ -2,28 +2,81 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"groupie/Mod"
+	"html/template"
 	"log"
 	"net/http"
+	"os/exec"
+	"runtime"
 )
 
+const tmpl = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Artist Info</title>
+	<link rel="stylesheet" type="text/css" href="/Styles/style.css">
+</head>
+<body>
+		{{range .}}
+		<article>
+		<div class="image">
+		<img src="{{.Image}}" alt="{{.Name}}">
+		</div>
+		<div>
+    	<h1>{{.Name}}</h1>
+		</div>
+		</article>
+		{{end}}
+</body>
+</html>
+`
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Appeler la fonction GetArtist depuis GroupieTracker.go
+	data, err := Mod.GetArtist()
+	if err != nil {
+		log.Fatalf("Erreur: %v", err)
+	}
+
+	// Charger le template HTML
+	tmpl, err := template.New("webpage").Parse(tmpl)
+	if err != nil {
+		log.Fatalf("Erreur lors du chargement du template: %v", err)
+	}
+
+	// Rendre le template avec les données
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Fatalf("Erreur lors du rendu du template: %v", err)
+	}
+}
+
+func openBrowser(url string) {
+	var err error
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+
+	if err != nil {
+		fmt.Printf("Failed to open browser: %v\n", err)
+	}
+}
+
 func main() {
-	// URL de l'API
-	url := "https://groupietrackers.herokuapp.com/api/artists"
-
-	// Faire une requête GET
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatalf("Erreur lors de la requête GET: %v", err)
+	http.Handle("/Styles/", http.StripPrefix("/Styles/", http.FileServer(http.Dir("Styles"))))
+	http.HandleFunc("/", handler)
+	fmt.Println("Starting server at port 8080")
+	go openBrowser("http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Server failed to start: %v\n", err)
 	}
-	defer resp.Body.Close()
-
-	// Lire le corps de la réponse
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Erreur lors de la lecture du corps de la réponse: %v", err)
-	}
-
-	// Afficher la réponse
-	fmt.Println(string(body))
 }
