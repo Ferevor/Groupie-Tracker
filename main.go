bf709bd2f446d2c8407c62b0ebc0a6285422ced3
ER1707 <ewan.17.remo@gmail.com>
2025-03-17 15:13:19 +0100

    réparation des filtres

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"groupie/Mod"
 	"html/template"
@@ -13,158 +14,70 @@ import (
 	"strings"
 )
 
-const tmpl = `
-<!DOCTYPE html>
-<html>
-	<head>
-    	<title>Artist Info</title>
-		<link rel="stylesheet" type="text/css" href="/Styles/style.css">
-	</head>
-	<body>
-		<div>
-			<div class="header">
-				<h1>Groupie Tracker</h1>
-				<form method="GET" action="/">
-					<select id="sort-select" name="sort">
-                		<option value="">Sort By</option>
-                		<option value="asc">Sort Ascending</option>
-                	    <option value="desc">Sort Descending</option>
-                	</select>
-					<select id="filter-select" name="filter">
-						<option value="">Filter By</option>
-						<option value="CreationDate">Creation Date</option>
-						<option value="FirstAlbum">First Album</option>
-					</select>
-					<input type="number" name="start" placeholder="De ">
-					<input type="number" name="end" placeholder="à ">
-					<button type="Submit">GO</button>
-
-				</form>
-			</div>
-			<div class="box">
-    			<form method="GET" action="/" id="searchForm">
-        			<input type="text" class="input" list="suggestionsquery" id="optionsList" name="displayQuery" value="{{.Query}}" autocomplete="off" placeholder=" "/>
-					<input type="hidden" id="searchQuery" name="query">
-					<datalist id="suggestionsquery">
-						{{range .OptionsSearchBar}}
-                        	<option value="{{.}}">{{.}}</option>
-                    {{end}}
-					</datalist>
-    			</form>
-			</div>
-			<div class="container">
-				{{if .No_results}}
-					<div class="no-results">
-						No results found
-					</div>
-				{{end}}
-				{{range .Data}}
-            		<label for="modal-{{.Name}}" class="button">
-						<div>
-                			<img src="{{.Image}}" alt="Image" width="200" height="200">
-						</div>
-						<div>
-                			<h2>{{.Name}}</h2>
-						</div>
-            		</label>
-            		<input type="checkbox" id="modal-{{.Name}}" class="modal-toggle">
-            		<div class="modal">
-                		<div class="modal-content">
-                    		<label for="modal-{{.Name}}" class="close">&times;</label>
-							<div>
-                   				<center> <h2>{{.Name}}</h2></center>
-   							</div>
-   							<div class ="image">
-                				<center><img src="{{.Image}}" alt="Image" width="192" height="192"></center>
-							</div>
-							<p>Members: {{range .Members}}{{.}}, {{end}}</p>
-							<p>Creation Date: {{.CreationDate}}</p>
-							<p>First Album: {{.FirstAlbum}}</p>
-							<p>Concert Location and Dates:</p>
-        					{{range $key, $value := .DatesLocations.DatesLocations}}
-							<p>{{$key}}</p>
-          					<ul>
-                				{{range $value}}
-                  				<li>{{.}}</li>
-                   				{{end}}
-            				</ul>
-        					{{end}}
-                		</div>
-        			</div>
-				{{end}}
-			</div>
-		</div>
-	</body>
-</html>
-`
-
 type PageData struct {
 	Query            string
 	Data             []Mod.Artist
 	OptionsSearchBar []string
+	CheckedOptions   []string
 	No_results       bool
 }
 
+var checkedOptions = []string{}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	displayQuery := r.URL.Query().Get("displayQuery")
 	query := r.URL.Query().Get("query")
+	displayQuery := r.URL.Query().Get("displayQuery")
+
 	data, err := Mod.GetData()
 	if err != nil {
-		log.Printf("Erreur lors de la récupération des données: %v", err)
-		http.Error(w, "Erreur lors de la récupération des données", http.StatusInternalServerError)
+		log.Printf("Error fetching data: %v", err)
+		http.Error(w, "Error fetching data", http.StatusInternalServerError)
 		return
 	}
 
-	//log.Printf("Données récupérées: %v", data)
-
-	sortOrder := r.URL.Query().Get("sort")
 	start := r.URL.Query().Get("start")
 	end := r.URL.Query().Get("end")
 
-	// Filtrer les artistes par date de création si les paramètres sont présents
-	if start != "" && end != "" { // Si les paramètres sont présents
-		startYear, err := strconv.Atoi(start) // Convertir les paramètres en entiers
+	if start != "" && end != "" {
+		startYear, err := strconv.Atoi(start)
 		if err != nil {
-			log.Printf("Erreur lors de la conversion de l'année de début: %v", err)
-			http.Error(w, "Erreur lors de la conversion de l'année de début", http.StatusInternalServerError)
+			log.Printf("Error converting start year to int: %v", err)
+			http.Error(w, "Error converting start year to int", http.StatusBadRequest)
 			return
 		}
 		endYear, err := strconv.Atoi(end)
 		if err != nil {
-			log.Printf("Erreur lors de la conversion de l'année de fin: %v", err)
-			http.Error(w, "Erreur lors de la conversion de l'année de fin", http.StatusInternalServerError)
+			log.Printf("Error converting end year to int: %v", err)
+			http.Error(w, "Error converting end year to int", http.StatusBadRequest)
 			return
 		}
 
-		var filteredData []Mod.Artist
+		var filteredArtists []Mod.Artist
 		switch r.URL.Query().Get("filter") {
 		case "CreationDate":
 			for _, artist := range data {
-				if artist.CreationDate >= startYear && artist.CreationDate <= endYear { //si l'année de création est comprise entre les années de début et de fin
-					filteredData = append(filteredData, artist) // Ajouter l'artiste à la liste des artistes filtrés
+				if artist.CreationDate >= startYear && artist.CreationDate <= endYear {
+					filteredArtists = append(filteredArtists, artist)
 				}
 			}
-			data = filteredData // Remplacer les données par les données filtrées
-			log.Printf("Données filtrées: %v", filteredData)
+			data = filteredArtists
 		case "FirstAlbum":
 			for _, artist := range data {
-				firstAlbumYear := strings.Split(artist.FirstAlbum, "-")   // Diviser la chaîne de l'année du premier album pour avoir seulement l'année
-				firstAlbumYearInt, err := strconv.Atoi(firstAlbumYear[2]) //convertir l'année en entier
+				firstAlbumYear := strings.Split(artist.FirstAlbum, "-")
+				firstAlbumYearInt, err := strconv.Atoi(firstAlbumYear[2])
 				if err != nil {
-					log.Printf("Erreur lors de la conversion de l'année du premier album: %v", err)
-					http.Error(w, "Erreur lors de la conversion de l'année du premier album", http.StatusInternalServerError)
+					log.Printf("Error converting first album year to int: %v", err)
+					http.Error(w, "Error converting first album year to int", http.StatusBadRequest)
 					return
 				}
-				if firstAlbumYearInt >= startYear && firstAlbumYearInt <= endYear { //si l'année du premier album est comprise entre les années de début et de fin
-					filteredData = append(filteredData, artist) // Ajouter l'artiste à la liste des artistes filtrés
+				if firstAlbumYearInt >= startYear && firstAlbumYearInt <= endYear {
+					filteredArtists = append(filteredArtists, artist)
 				}
 			}
-			data = filteredData // Remplacer les données par les données filtrées
-			log.Printf("Données filtrées: %v", filteredData)
+			data = filteredArtists
 		}
 	}
-
-	// Trier les artistes si le paramètre de tri est présent
+	sortOrder := r.URL.Query().Get("sort")
 	if sortOrder == "asc" {
 		sort.Slice(data, func(i, j int) bool {
 			return data[i].Name < data[j].Name
@@ -175,50 +88,72 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		})
 	}
 
-	//filteredArtists := Mod.SearchBar(query, data)
+	filteredArtists := Mod.SearchBarCheckBox(checkedOptions, query, data)
 	optionsSearchBar := Mod.SearchOptions(query, data)
 
 	var no_results bool
-	if len(data) == 0 {
+	if len(filteredArtists) == 0 {
 		no_results = true
-		log.Printf("Aucun résultat trouvé")
-	} else {
-		log.Printf("Des résultats ont été trouvés")
 	}
-
-	// Charger le template HTML
-	tmpl, err := template.New("webpage").Parse(tmpl)
-	if err != nil {
-		log.Printf("Erreur lors du chargement du template: %v", err)
-		http.Error(w, "Erreur lors du chargement du template", http.StatusInternalServerError)
-		return
-	}
-
-	log.Printf("Valeur de no_results: %v", no_results)
 
 	pageData := PageData{
 		Query:            displayQuery,
-		Data:             data,
+		Data:             filteredArtists,
 		OptionsSearchBar: optionsSearchBar,
+		CheckedOptions:   checkedOptions,
 		No_results:       no_results,
 	}
 
-	// Rendre le template avec les données
-	err = tmpl.Execute(w, pageData)
-
+	t := template.New("GroupTra.tmpl").Funcs(template.FuncMap{
+		"contains": Mod.Contains,
+	})
+	t, err = t.ParseFiles("Templates/GroupTra.tmpl")
 	if err != nil {
-		log.Printf("Erreur lors du rendu du template: %v", err)
-		http.Error(w, "Erreur lors du rendu du template", http.StatusInternalServerError)
+		log.Printf("Error loading template: %v", err)
+		http.Error(w, "Error loading template", http.StatusInternalServerError)
 		return
 	}
+
+	err = t.Execute(w, pageData)
+	if err != nil {
+		log.Printf("Error rendering template: %v", err)
+		http.Error(w, "Error rendering template", http.StatusInternalServerError)
+		return
+	}
+}
+
+func getCheckedOptionsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string][]string{"checkedOptions": checkedOptions})
+}
+
+func updateCheckedOptionsHandler(w http.ResponseWriter, r *http.Request) {
+	var selection struct {
+		Option    string `json:"option"`
+		IsChecked bool   `json:"isChecked"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&selection); err != nil {
+		http.Error(w, "Invalid data", http.StatusBadRequest)
+		return
+	}
+
+	if selection.IsChecked {
+		if !Mod.Contains(checkedOptions, selection.Option) {
+			checkedOptions = append(checkedOptions, selection.Option)
+		}
+	} else {
+		checkedOptions = Mod.RemoveFromCheckedOptions(checkedOptions, selection.Option)
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func searchOptionsHandler(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query().Get("q")
 	data, err := Mod.GetData()
 	if err != nil {
-		log.Printf("Erreur lors de la récupération des données: %v", err)
-		http.Error(w, "Erreur lors de la récupération des données", http.StatusInternalServerError)
+		log.Printf("Error fetching data: %v", err)
+		http.Error(w, "Error fetching data", http.StatusInternalServerError)
 		return
 	}
 	optionsSearchBar := Mod.SearchOptions(query, data)
@@ -235,7 +170,6 @@ func searchOptionsHandler(w http.ResponseWriter, r *http.Request) {
 
 func openBrowser(url string) {
 	var err error
-
 	switch os := runtime.GOOS; os {
 	case "linux":
 		err = exec.Command("xdg-open", url).Start()
@@ -246,7 +180,6 @@ func openBrowser(url string) {
 	default:
 		err = fmt.Errorf("unsupported platform")
 	}
-
 	if err != nil {
 		fmt.Printf("Failed to open browser: %v\n", err)
 	}
@@ -256,11 +189,12 @@ func main() {
 	http.Handle("/Styles/", http.StripPrefix("/Styles/", http.FileServer(http.Dir("Styles"))))
 	http.Handle("/Scripts/", http.StripPrefix("/Scripts/", http.FileServer(http.Dir("Scripts"))))
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/get-checked-options", getCheckedOptionsHandler)
+	http.HandleFunc("/update-checked-options", updateCheckedOptionsHandler)
 	http.HandleFunc("/search", searchOptionsHandler)
 	fmt.Println("Starting server at port 8080")
 	go openBrowser("http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Printf("Server failed to start: %v\n", err)
-		return
 	}
 }
